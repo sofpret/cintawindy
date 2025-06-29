13bff0847d71fca7b08283b17dfa797c9b75c224
sofpret <59215603+sofpret@users.noreply.github.com>
2025-06-30 00:37:58 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const total = 20;
+  for (let i = 0; i < total; i++) {
+    const flower = document.createElement("div");
+    flower.className = "flower";
+    flower.style.left = `${Math.random() * 100}vw`;
+    flower.style.animationDuration = `${5 + Math.random() * 5}s`;
+    document.body.appendChild(flower);
+  }
+});
 
